c81cf44e01f076876cc03658862a4ce920f3be5d
Kunal Mahajan <kunalvineeta5@gmail.com>
2018-03-06 02:34:15 +0530

    Add project page

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.use((req,res,next)=>{
   });
   next();
 });
-app.use((req,res,next)=>{
-  res.render('maintenance.hbs');
-});
+// app.use((req,res,next)=>{
+//   res.render('maintenance.hbs');
+// });
 app.use(express.static(__dirname+'/public'));
 hbs.registerHelper('getCurrentYear', ()=>{
   return new Date().getFullYear();
@@ -45,6 +45,11 @@ app.get('/',(req,res)=>{
 app.get('/about',(req,res)=>{
   res.render('about.hbs',{
     pageTitle:'About page'
+  });
+});
+app.get('/projects',(req,res)=>{
+  res.render('projects.hbs',{
+    pageTitle:'Projects'
   });
 });
 app.get('/bad',(req,res)=>{
